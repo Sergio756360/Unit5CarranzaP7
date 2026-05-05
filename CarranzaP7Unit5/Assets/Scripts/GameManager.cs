143612b827e350addb35a210746d062050286a9c
Sergio756360 <756360@student.dusd.net>
2026-05-05 15:25:35 -0700

    Score text
    added score text onto the project

---
+++ CarranzaP7Unit5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    private int score;
+    private float spawnRate = 1.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        scoreText.text = "Score:" + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+        
+    }
+}
