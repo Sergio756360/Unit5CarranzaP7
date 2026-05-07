7edbb6f18e382c812950b3284d79df9c647fa90d
Sergio756360 <756360@student.dusd.net>
2026-05-07 15:25:46 -0700

    Added the Game Over Text and Restart text
    I added the Game Over Text and Restart Text and Button to the game, soon I will be done with this project by adding the difficulties such as Easy, Medium and Hard.

--- CarranzaP7Unit5/Assets/Scripts/GameManager.cs
+++ CarranzaP7Unit5/Assets/Scripts/GameManager.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(SpawnTarget());
+        isGameActive = true;
         score = 0;
-        scoreText.text = "Score:" + score;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+
     }
 
     // Update is called once per frame
@@ -27,12 +35,31 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
         
+    }
+
+   public void UpdateScore (int scoreToAdd)
+   {
+     score += scoreToAdd;
+     scoreText.text = "Score:" + score;
+   }
+
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+      
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
--- CarranzaP7Unit5/Assets/Scripts/Target.cs
+++ CarranzaP7Unit5/Assets/Scripts/Target.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -2;
+    private float ySpawnPos = -6;
+
+    public ParticleSystem explosionParticle;
+    public int pointValue;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque (), RandomTorque (),ForceMode.Impulse);
@@ -28,12 +33,23 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
     }
 
     private void OnTriggerEnter (Collider other)
     {
         Destroy(gameObject);
+
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+        
     }
 
     private void OnMouseUp()
